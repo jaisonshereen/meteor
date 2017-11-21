4557c6898610c5f3ff986ace7bd3664101a87686
Jaison peter <jaisonpeter.m@sonata-software.com>
2017-11-21 17:35:37 +0530

    FirstChat
    Added Chat application initial files

---
+++ client/models.js
@@ -0,0 +1,16 @@
+/**
+* Models
+*/
+Messages = new Meteor.Collection('messages');
+
+Messages.allow({
+         insert: function () {
+         return true;
+         },
+         update: function () {
+         return true;
+         },
+         remove: function () {
+         return true;
+         }
+         });
